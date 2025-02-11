f9b6b99c94976f500cf7b6cd118af9f23ce912c3
VeerababuY <veerababu.vvs@gmail.com>
2025-02-11 15:25:42 +0530

    Add files via upload
    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+
+app.use("/", (req, res, next) => {
+    res.send("This is the express server")
+})
+
+
+app.get("/hello", (req, res, next) => {
+    res.send("This is the hello response");
+})
+
+
+app.listen(3000, () => {
+    console.log("Server is Running")
+})
